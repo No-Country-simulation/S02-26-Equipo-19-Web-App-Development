3d1ea67671a00343b33a56253876e10c8ddeab55
feichulin <feimosqueda777@gmail.com>
2026-02-12 18:03:21 -0300

    hice un boton de logOut con su funcion de llevate a  /login

---
+++ frontend/CareConnect/src/components/common/LogOutButton.jsx
@@ -0,0 +1,18 @@
+import { LogOut } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+const LogOutButton = () => {
+    const navigate = useNavigate();
+    const hadleLogOut =  () =>{
+        navigate("/login")
+    }
+    return (
+        <button
+            className="p-1 flex items-center gap-2 text-f-secondary rounded-lg cursor-pointer hover:bg-border hover:text-f-primary transition-all"
+            onClick={hadleLogOut}
+        >
+            <LogOut className=" w-6  sm:w-4" />
+            <p className="font-semibold hidden sm:block ">Cerrar Sesion</p>
+        </button>
+    );
+};
+export default LogOutButton;
