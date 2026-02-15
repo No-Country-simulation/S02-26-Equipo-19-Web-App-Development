e2b00e246bb03faaf048185ba25e7759dcab92de
YoshuaPariona <loganpariona.1207@gmail.com>
2026-02-14 23:09:13 -0500

    feat: Mapeo completo del dominio de Onboarding

--- backend/src/main/java/com/team19/CareConnect/onboarding/domain/CaregiverRequest.java
+++ backend/src/main/java/com/team19/CareConnect/onboarding/domain/CaregiverRequest.java
@@ -7,9 +7,11 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
-import org.springframework.data.annotation.CreatedDate;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Table(name = "caregiver_request")
 @Entity
 @Setter @Getter
@@ -22,24 +24,58 @@ public class CaregiverRequest {
 
     @Enumerated(EnumType.STRING)
     @JdbcTypeCode(SqlTypes.NAMED_ENUM)
-    @Column(name = "application_status", columnDefinition = "application_status")
-    private ApplicationStatus applicationStatus = ApplicationStatus.PENDING;
+    @Column(name = "application_status", columnDefinition = "application_status", nullable = false)
+    private ApplicationStatus applicationStatus;
 
-    @CreatedDate
-    @Column(name = "submitted_at")
+    @Column(name = "submitted_at", updatable = false)
     private OffsetDateTime submittedAt;
 
     @Column(name = "reviewed_at")
     private OffsetDateTime reviewedAt;
 
-
-    @Column(name = "rejection_reason", columnDefinition = "TEXT")
+    @Column(name = "rejection_reason")
     private String rejectionReason;
 
-    @Column(name = "spread_sheet_id", columnDefinition = "TEXT")
+    @Column(name = "spread_sheet_id")
     private String spreadSheetId;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "reviewed_by")
     private Admin reviewedBy;
+
+    // Relación con Document
+    @OneToMany(mappedBy = "application")
+    private List<Document> documents = new ArrayList<>();
+
+    // Relación con FormInformation
+    @OneToOne(mappedBy = "application",
+            fetch = FetchType.EAGER,
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+            optional = false
+    )
+    private FormInformation formInformation;
+
+    // Valores por defecto
+    @PrePersist
+    private void prePersist() {
+        if(submittedAt == null) {
+            submittedAt = OffsetDateTime.now();
+        }
+        if(applicationStatus == null) {
+            applicationStatus = ApplicationStatus.PENDING;
+        }
+    }
+
+    // Helpers
+    public void addDocument(Document document) {
+        this.documents.add(document);
+        document.setApplication(this);
+    }
+
+    public void setFormInformation(FormInformation formInformation) {
+        this.formInformation = formInformation;
+        if(formInformation != null) {
+            formInformation.setApplication(this);
+        }
+    }
 }
