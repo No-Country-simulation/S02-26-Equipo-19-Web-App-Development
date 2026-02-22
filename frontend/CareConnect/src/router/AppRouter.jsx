ab07a238dc45c9659f36f1567e48c2cfaec384e1
adriel9-5 <adriel39102248@gmail.com>
2026-02-22 01:47:31 -0300

    feat: implementacion de enrutamiento y sidebar

---
+++ frontend/CareConnect/src/router/AppRouter.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import AdminLayout from "../pages/admin/layouts/AdminLayout";
+import AdminHome from "../pages/admin/AdminHome";
+import AdminUsers from "../pages/admin/AdminUsers";
+import AdminPatients from "../pages/admin/AdminPatients";
+import AdminCaregivers from "../pages/admin/AdminCaregivers";
+
+import Home from "../pages/home/Home";
+import Family from "../pages/family/Family";
+import Caregivers from "../pages/caregiver/Caregivers";
+
+const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+
+                {/* Public routes */}
+                <Route path="/" element={<Home />} />
+                <Route path="/family" element={<Family />} />
+                <Route path="/caregivers" element={<Caregivers />} />
+
+                {/* Admin routes */}
+                <Route path="/admin" element={<AdminLayout />}>
+                    <Route index element={<AdminHome />} />
+                    <Route path="users" element={<AdminUsers />} />
+                    <Route path="patients" element={<AdminPatients />} />
+                    <Route path="caregivers" element={<AdminCaregivers />} />
+                </Route>
+
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default AppRouter;
