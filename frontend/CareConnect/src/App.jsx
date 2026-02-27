c31d225d3b45123c9edbd588d705448be4251b2c
Ezequiel Oliver <ezequiel.oliver@live.com.ar>
2026-02-27 17:44:21 -0300

    fix: add ToastContainer in App.jsx

--- frontend/CareConnect/src/App.jsx
+++ frontend/CareConnect/src/App.jsx
@@ -1,11 +1,14 @@
-
+import { ToastContainer } from "react-toastify";
 import AppRouter from "./router/AppRouter";
 
 function App() {
 
 
   return (
+    <>
     <AppRouter/>
+    <ToastContainer position="top-right" autoClose={3000} />
+    </>
   )
 }
 
